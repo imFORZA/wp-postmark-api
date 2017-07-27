863cace5e3893dacea7fd6af96ae4186de923049
Brandon Hubbard <bkhubbard@gmail.com>
2017-07-27 16:40:54 -0700

    more functions

--- wp-postmark-status-api.php
+++ wp-postmark-status-api.php
@@ -1,3 +1,63 @@
 <?php
+/**
+ * WP Postmark API (http://developer.postmarkapp.com/)
+ *
+ * @package WP-API-Libraries\WP-Postmark-Base\WP-Postmark-Status-API
+ */
 
-// https://status.postmarkapp.com/api
+
+/* Exit if accessed directly. */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+/* Check if class exists. */
+if ( ! class_exists( 'PostMarkStatusAPI' ) ) {
+
+
+	/**
+	 * PostMarkStatusAPI class.
+	 */
+	class PostMarkStatusAPI extends PostMarkBase{
+
+
+		/**
+		 * Status URL
+		 * Docs: https://status.postmarkapp.com/api/
+		 *
+		 * (default value: 'https://status.postmarkapp.com/api/')
+		 *
+		 * @var string
+		 * @access protected
+		 */
+		protected $spamcheck_uri = 'https://status.postmarkapp.com/api/1.0';
+
+
+		public function get_status() {
+			// /status
+		}
+
+		public function get_last_incident() {
+
+		}
+
+		public function list_incidents() {
+
+		}
+
+		public function get_incident( $incident_id ) {
+
+		}
+
+		public function get_services_status() {
+			// /services
+		}
+
+		public function get_services_availability() {
+			// /services/availability
+		}
+
+		public function get_delivery_stats() {
+			// /delivery
+		}
+	}
+
+}
