223cb8059481a0f1f6d046d02505d5c391a6075a
Brandon Hubbard <bkhubbard@gmail.com>
2017-07-27 12:44:44 -0700

    empty functions
    Saving @bradleymoore111 some time

--- wp-postmark-dmarc-api.php
+++ wp-postmark-dmarc-api.php
@@ -29,6 +29,50 @@ if ( ! class_exists( 'PostMarkDmarcAPI' ) ) {
 		protected $dmark_uri = 'https://dmarc.postmarkapp.com/api/';
 
 
+		/**
+		 * create_record function.
+		 *
+		 * @access public
+		 * @param mixed $email
+		 * @param mixed $domain
+		 * @return void
+		 */
+		public function create_record( $email, $domain ) {
+
+		}
+
+		public function get_record() {
+
+		}
+
+		public function get_dns_snippet() {
+
+		}
+
+		public function verify_dns() {
+
+		}
+
+		public function delete_record() {
+
+		}
+
+		public function list_dmarc_reprots( $from_date = '', $to_date = '', $limit = '', $after = '' ) {
+
+		}
+
+		public function get_dmarc_report( $dmarc_report_id ) {
+
+		}
+
+		public function recover_api_token( $owner ) {
+			// tokens/recover
+		}
+
+		public function rotate_api_token() {
+			// /records/my/token/rotate
+		}
+
 	}
 
 }
